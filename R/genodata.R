d59b4774d1ee0a2c752ee35d1b27ea19331b0f77
Florent Chuffart <florent.chuffart@gmail.com>
2016-04-12 17:45:43 +0200

    Adding genodata features

---
+++ R/genodata.R
@@ -0,0 +1,48 @@
+#' A Function That Generates a Genome.
+#'
+#' This function generates a genome around significant parental alleles.
+#' @param alleles A vector that describes significant parental alleles (those that will control your phenotype
+#' @param half_chr_width An integer that defines the length of the (half (chromosome size minus 1)). 
+#' @param teta An interger that specify recombination factor to use.
+#' @examples
+#' alleles = sample(c("BY","RM"), 25, replace=TRUE)
+#' print(alleles)
+#' genodata = generate_genodata(alleles, half_chr_width=50)
+#' print(genodata)
+#' tetas = compute_teta(genodata)
+#' # matlab::imagesc(t(as.matrix(genodata)), xlab="markers", ylab="segrgants", main="Parental origin of segregant genomes")
+#' plot(density(tetas), main=paste("Recombination Fraction Distribution"), xlab=paste("mean(teta)=", signif(mean(tetas),3), sep=""))
+#' abline(v=mean(tetas), lty=2)
+#' @export
+generate_genodata = function(alleles, half_chr_width = 100, teta=0.05) {
+  nb_indiv = length(alleles)
+  chr_w = 2 * half_chr_width + 1
+
+  alleles = as.numeric(as.factor(alleles))
+  all_ref = 1
+  all = (alleles == all_ref) + 1 %% 2
+  g = matrix(rep(0,nb_indiv*chr_w), nb_indiv)
+  g[, half_chr_width + 1] = (all==1)
+  for(i in (half_chr_width + 2):chr_w) {
+    g[, i] = (g[, i-1] + (runif(nb_indiv) < teta)) %% 2   
+  }
+  for(i in (half_chr_width):1) {
+    g[, i] = (g[, i+1] + (runif(nb_indiv) < teta)) %% 2   
+  }
+  genome_matrix = g
+  genodata = data.frame(t(genome_matrix))
+  names(genodata) = paste("seg", 1:nb_indiv, sep="_")
+  return(genodata)
+}
+
+#' A Function That Computes Recombination Factors.
+#'
+#' This function computes recombination factors between markers.
+#' @param genodata A genome generated with the function `generate_genodata`
+#' @export
+compute_teta = function(genodata) {
+  genome_matrix = t(as.matrix(genodata))
+  tetas = apply(genome_matrix[,-1] != genome_matrix[,-ncol(genome_matrix)], 2, sum) / nrow(genome_matrix)
+  return(tetas)  
+}
+
